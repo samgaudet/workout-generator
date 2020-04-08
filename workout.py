c013be5ed0277cce82ddf68d4adf51c89a46de81
Sam Gaudet <sgaudet1@babson.edu>
2020-04-08 11:50:03 -0400

    update with choosing exercises and main methods

--- workout.py
+++ workout.py
@@ -29,7 +29,36 @@ class Workout:
 
         for workout_group in self.workout_groupings:
             workouts = list(self.exercises[workout_group].keys())
-            workout = workouts[random.randint(0, len(workouts))]
+            workout = workouts[random.randint(0, len(workouts)-1)]
             self.workouts[workout_group] = workout
 
-    # def
+    def create_workout(self):
+
+        self.routine = ""
+
+        for workout_group in list(self.workouts.keys()):
+
+            exercise = self.workouts[workout_group]
+            exercise_name = self.exercises[workout_group][exercise]["name"]
+
+            # if the exercise is not a rest day, get the number and units of the exercise
+            if exercise != "rest_day":
+                number_of_units = random.randint(
+                    self.exercises[workout_group][exercise]["min"], self.exercises[workout_group][exercise]["max"])
+                units = self.exercises[workout_group][exercise]["unit"]
+
+            # otherwise the exercise is
+            else:
+                number_of_units = ""
+                units = ""
+
+            routine_string = f"{exercise_name}: {number_of_units} {units}\n"
+            self.routine += routine_string
+
+    def main(self):
+
+        self.open_json_from_filepath()
+        self.get_workout_groupings_from_json()
+        self.choose_exercises()
+        self.create_workout()
+        print(self.routine)
